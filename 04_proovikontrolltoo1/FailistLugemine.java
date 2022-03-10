b21706e59b694c0b9b18322a8f08f26e9b97346e
tonispold <tonis123@tlu.ee>
2022-03-10 11:59:51 +0200

    Esimeselt kontrolltööks valmistumine

---
+++ 04_proovikontrolltoo1/FailistLugemine.java
@@ -0,0 +1,18 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.List;
+
+public class FailistLugemine {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new FileReader("kirjeldus.txt"));
+        String rida = br.readLine();
+        System.out.println(rida);
+        List<String> tykeldus = List.of(rida.split("es"));
+        System.out.println(tykeldus);
+        System.out.println(tykeldus.size());
+        System.out.println(Math.sqrt(tykeldus.size()));
+        System.out.println(Math.pow(tykeldus.size(),3));
+    }
+}
---
+++ 04_proovikontrolltoo1/Hulknurk.java
@@ -0,0 +1,14 @@
+public class Hulknurk {
+    public static void main(String[] args) {
+        Klass klass = new Klass();
+        klass.setxCoords(3,4,5);
+        klass.setyCoords(5,6,7);
+
+        Klass klass2 = new Klass();
+        klass2.setxCoords(3,6,87);
+        klass2.setyCoords(6,4,7);
+
+        System.out.println(klass);
+        System.out.println(klass2);
+    }
+}
